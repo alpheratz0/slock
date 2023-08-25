ec5a6a6b0ddce194c884e60794265cf32fa8f8d3
alpheratz0 <alpheratz99@protonmail.com>
2023-08-24 22:27:34 -0300

    change theme

--- config.def.h
+++ config.def.h
@@ -4,8 +4,8 @@ static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INPUT] =  "black",     /* during input */
+	[FAILED] = "#ad2a4b",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
